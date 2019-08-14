9f11fe0d9bdbc8effba4fa086c15e83283c01380
Aurelie Mahe <mahe.aureliems@gmail.com>
2019-08-14 17:10:28 +0200

    Fix image display issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "destroying all flats..."
 Flat.destroy_all
 
-Flat.create!(
+puts "Creating 4 flats..."
+flat1 = Flat.create!(
   name: 'Light & Spacious Garden Flat London',
   address: '10 Clifton Gardens London W9 1DT',
   picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200',
@@ -16,8 +18,9 @@ Flat.create!(
   price_per_night: 75,
   number_of_guests: 3
 )
+p flat1.picture_url
 
-Flat.create!(
+flat2 = Flat.create!(
   name: 'Stylish House Close to River Thames',
   address: '5 Queensmill Road London SW6 6JP',
   picture_url: 'https://images.unsplash.com/photo-1493809842364-78817add7ffb?w=1200',
@@ -25,8 +28,9 @@ Flat.create!(
   price_per_night: 65,
   number_of_guests: 2
 )
+p flat2.picture_url
 
-Flat.create!(
+flat3 = Flat.create!(
   name: 'St Pancras Clock Tower Guest Suite',
   address: 'Euston Rd London N1C 4QP',
   picture_url: 'https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?w=1200',
@@ -34,8 +38,9 @@ Flat.create!(
   price_per_night: 110,
   number_of_guests: 2
 )
+p flat3.picture_url
 
-Flat.create!(
+flat4 = Flat.create!(
   name: 'Whole Flat one minute from Kensington Olympia',
   address: 'Guild Street London SE13 7XD',
   picture_url: 'https://source.unsplash.com/uY2kic9wlmc/1200x800',
@@ -43,3 +48,6 @@ Flat.create!(
   price_per_night: 108,
   number_of_guests: 4
 )
+p flat4.picture_url
+
+puts "Created 4 flats"
